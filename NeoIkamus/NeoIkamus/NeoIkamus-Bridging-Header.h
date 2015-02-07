81e991ab5a030844512d49c69ea79ed593259e09
Riza <riza.alaudin@kii.com>
2015-02-07 17:01:31 +0900

    init projects

---
+++ NeoIkamus/NeoIkamus/NeoIkamus-Bridging-Header.h
@@ -0,0 +1,6 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <KiiSDK/KiiSDK-Bridging-Header.h>
+#import <KiiAnalytics/KiiAnalytics-Bridging-Header.h>
